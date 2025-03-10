ca8008f74af5d9e27b991806b3ba5a66fdfbbdbe
jocori <jocori@ku.edu>
2025-03-10 11:55:54 -0500

    finished preliminary sex update function with clean code

--- code/sex_function.R
+++ code/sex_function.R
@@ -1,7 +1,9 @@
 # Load necessary library
 library(dplyr)
 setwd("~/Desktop/KU/Projects/BirdData")
-
+sex<- read.csv("data/NAOC_sex_mappings_JLC_NL25.csv")
+sex<-sex[,-c(12:28)]
+head(sex)
 # Define the classification function
 classify_sex <- function(value) {
   # Save the original value for count extraction
@@ -50,24 +52,73 @@ classify_sex <- function(value) {
   mixed_found <-regmatches(value,mixed_match)[[1]]
   
   #if then statements for mixed,male,female,atypical,unknown, indeterminate; unspecified for everything not captured by all other patterns
-  if (length(male_found) > 0) & (length(female_found) > 0) | 
-      (length(male_found) >1) | (length(female_found) >1)) & (length(mixed_found)>0){
+  if ((length(male_found) > 0 & length(female_found) > 0) | 
+      (length(male_found) > 1) | 
+      (length(female_found) > 1) |
+      (length(mixed_found) > 0)) {
     sex_category <- "MIXED"
-  } else if 
-    (length(male_found) == 1){
-    sex_category <= "MALE"
-    } else if (length(female_found) == 1){
-    sex_category == "FEMALE"
-    } else if (length(atypical_found) == 1){
+  } else if (length(male_found) == 1) {
+    sex_category <- "MALE"
+  } else if (length(female_found) == 1) {
+    sex_category <- "FEMALE"
+  } else if (length(atypical_found) == 1) {
     sex_category <- "ATYPICAL"
-    } else if (length(unknown_found) == 1){
-    sex_catergory <- "UNKNOWN"
-  } else 
+  } else if (length(unknown_found) == 1) {
+    sex_category <- "UNKNOWN"
+  } else if (length(indeterminate_found) == 1) {
+    sex_category <- "INDETERMINATE"
+  } else {
+    sex_category <- "UNSPECIFIED"
+  }
+  
+  #Add notes with counts and extra information
+
+  notes_parts <- c()
+  # 1. Extract numeric counts with associated sex terms from the original value.
+  count_pattern <- "(?i)\\b\\d+\\s*(?:MALE|FEMALE|MACHO|MASCLE|FEMELLE|HEMBRA)S?\\b"
+  count_matches <- unlist(regmatches(original_value, gregexpr(count_pattern, original_value, perl=TRUE)))
+  
+  # Add counts to notes explicitly for MIXED cases
+  if (sex_category == "MIXED") {
+    if (length(count_matches) > 0) {
+      notes_parts <- c(notes_parts, paste(count_matches, collapse="; "))
+    }
+    
+  } else {
+    if (length(count_matches) > 0) {
+      notes_parts <- c(notes_parts, paste(count_matches, collapse="; "))
+    }
+  }
+  # 3. Capture extra text after removing recognized tokens
+  extra_text <- value
+  extra_text <- gsub(male_pattern, "", extra_text, perl=TRUE)
+  extra_text <- gsub(female_pattern, "", extra_text, perl=TRUE)
+  extra_text <- gsub(count_pattern, "", extra_text, ignore.case=TRUE, perl=TRUE)
+  extra_text <- gsub("[[:punct:]]", " ", extra_text)  # remove punctuation
+  extra_text <- trimws(extra_text)
+  
+  if (nchar(extra_text) > 0) {
+    notes_parts <- c(notes_parts, extra_text)
+  }
+  
+  final_notes <- if (length(notes_parts) > 0) paste(notes_parts, collapse="; ") else NA
   
   
+  return(c(sex_category, final_notes))
+}
+data <- sex %>% 
+  rowwise() %>% 
+  mutate(
+    result = list(classify_sex(uppercase.value)),
+    JLC.working = result[[1]],
+    notes = result[[2]]
+  ) %>% 
+  ungroup() %>% 
+  select(-result)
 #MIXED WILL BE RECORDS THAT HAVE MULTIPLE TERMS IN SEX COLUMN
   
   #INDETERMINATE RECORDS CONTAIN A QUESTION MARK
   #UNSPECIFIED WILL CAPTURE WORDS THAT ARE NOT INCLUDED IN DEFINED TERMS (SO EVERYTHING ELSE)
+#remove numbers at end for notes for "mixed", check terms, make sure jlc.working matches updated.mapping and maps.to.sex.concept
   
   

f8e4b895a9472cb38b6ef1832034cc679e89a98d
jocori <jocori@ku.edu>
2025-04-14 11:37:35 -0500

    updated functionality to handle special cases

--- code/sex_function.R
+++ code/sex_function.R
@@ -1,118 +1,78 @@
 # Load necessary library
 library(dplyr)
 setwd("~/Desktop/KU/Projects/BirdData")
-sex<- read.csv("data/NAOC_sex_mappings_JLC_NL25.csv")
-sex<-sex[,-c(12:28)]
-head(sex)
-# Define the classification function
+sex <- read.csv("data/NAOC_sex_mappings_JLC_NL25.csv")
+sex <- sex[,-c(12:28)]
+
+# Revised classify_sex function
 classify_sex <- function(value) {
-  # Save the original value for count extraction
   original_value <- value
-  
-  # Standardize text: uppercase and trim whitespace
   value <- toupper(trimws(value))
   
-  # Return "blank" if the value is empty
-  if (value == "") {
-    return(c("blank", NA))
-  }
-  # Define translation table vectors for male and female across languages REVISE JO
-  male_words <- c("MALE", "MACHO", "MASCLE", "HOMME", "HOMBRE", 
-                  "MASCULINO", "MASCULIN", "MASCULÍ", "MASCULÍN", "MASCULÍNE", "MASCHIO","HANE","MÂLE","TESTES","M")
-  female_words <- c("FEMALE", "FEMELLE", "HEMBRA", "FEMME", 
-                    "MUJER", "FÉMININE", "FEMENINO", "FEMININ", "FEMELLA", "DONNA","FÊEMA",
-                    "FÊMA","FEMEA","FÊMEA","FÍMEA","F")
-  unknown_words<-c("INDETERMINADOS","INDETERMINATE","O? Filhote","NON DÉT","SEXE INDÉTERMINÉ","INDEFINIDO",
-                   "DESCONEGUT","UNKNOWN","UNDETERMINED","UNSEXED","UNKNOW","SEX?")
-  atypical_words<-c("INTERSEX","GYNANDROMORPH","HERMAPHRODITE","M=F","MACHO Y HEMBRA","MIXED")
-  indeterminate_words<-c("UNCLEAR", "UNSURE", "POSSIBLE", "QUESTIONABLE", "AMBIGUOUS")
-  combined_words<-c(male_words, female_words)
+  if (value == "") return(c("blank", NA))
   
-  #create regex patterns for male, female, unknown, atypical, indeterminate
+  male_words <- c("MALE", "MACHO", "MASCLE", "HOMME", "HOMBRE", 
+                  "MASCULINO", "MASCULIN", "MASCULÍ", "MASCULÍN", "MASCULÍNE",
+                  "MASCHIO","HANE","MÂLE","TESTES","M")
+  female_words <- c("FEMALE", "FEMELLE", "HEMBRA", "FEMME", 
+                    "MUJER", "FÉMININE", "FEMENINO", "FEMININ", "FEMELLA",
+                    "DONNA","FÊEMA","FÊMA","FEMEA","FÊMEA","FÍMEA","F")
+  unknown_words <- c("INDETERMINADOS","INDETERMINATE","O? FILHOTE","NON DÉT",
+                     "SEXE INDÉTERMINÉ","INDEFINIDO","DESCONEGUT","UNKNOWN",
+                     "UNDETERMINED","UNSEXED","UNKNOW","SEX?")
+  atypical_words <- c("INTERSEX","GYNANDROMORPH","HERMAPHRODITE","M=F","MACHO Y HEMBRA","MIXED")
+  indeterminate_words <- c("UNCLEAR", "UNSURE", "POSSIBLE", "QUESTIONABLE", "AMBIGUOUS")
+  
+  combined_words <- c(male_words, female_words)
+  
   male_pattern <- paste0("\\b(", paste(male_words, collapse="|"), ")\\b(?!\\?)|\\bM\\b")
   female_pattern <- paste0("\\b(", paste(female_words, collapse="|"), ")\\b(?!\\?)|\\bF\\b")
+  attached_count_pattern <- "(?i)^(\\d+)(MALE|FEMALE|MACHO|MASCLE|FEMELLE|HEMBRA)S?$"
   unknown_pattern <- paste0("\\b(", paste(unknown_words, collapse="|"), ")\\b(?!\\?)|\\bU\\b")
   atypical_pattern <- paste0("\\b(", paste(atypical_words, collapse="|"), ")\\b(?!\\?)|\\bA\\b")
-  indeterminate_pattern <- paste0("\\b(", paste(indeterminate_words, collapse="|"), ")\\b|\\?")
+  indeterminate_pattern <- paste0("\\b(", paste(indeterminate_words, collapse = "|"), ")\\b|\\?|:\\s*\\?")
   mixed_pattern <- paste0("\\b\\d+\\s*(", paste(combined_words, collapse="|"), ")\\b")
+  single_letter_sex_pattern <- "^\\s*([MF])(?:\\s+\\1)+\\s*$"
   
-  # Look for matches of male and female patterns in the value
-  male_match <- gregexpr(male_pattern, value, perl=TRUE)
-  female_match <- gregexpr(female_pattern, value, perl=TRUE)
-  unknown_match <-gregexpr(unknown_pattern,value,perl=TRUE)
-  atypical_match <-gregexpr(atypical_pattern,value,perl=TRUE)
-  indeterminate_match <-gregexpr(indeterminate_pattern,value,perl=TRUE)
-  mixed_match <- gregexpr(mixed_pattern,value,perl=TRUE)
+  attached_found <- regmatches(value, regexec(attached_count_pattern, value, perl=TRUE))[[1]]
+  single_letter_found <- regmatches(value, regexec(single_letter_sex_pattern, value, perl=TRUE))[[1]]
   
-  male_found <- regmatches(value, male_match)[[1]]
-  female_found <- regmatches(value, female_match)[[1]]
-  unknown_found<-regmatches(value,unknown_match)[[1]]
-  atypical_found<-regmatches(value,atypical_match)[[1]]
-  indeterminate_found<-regmatches(value,indeterminate_match)[[1]]
-  mixed_found <-regmatches(value,mixed_match)[[1]]
+  male_found <- length(regmatches(value, gregexpr(male_pattern, value, perl=TRUE))[[1]])
+  female_found <- length(regmatches(value, gregexpr(female_pattern, value, perl=TRUE))[[1]])
+  unknown_found <- length(regmatches(value, gregexpr(unknown_pattern, value, perl=TRUE))[[1]])
+  atypical_found <- length(regmatches(value, gregexpr(atypical_pattern, value, perl=TRUE))[[1]])
+  indeterminate_found <- length(regmatches(value, gregexpr(indeterminate_pattern, value, perl=TRUE))[[1]])
+  mixed_found <- length(regmatches(value, gregexpr(mixed_pattern, value, perl=TRUE))[[1]])
   
-  #if then statements for mixed,male,female,atypical,unknown, indeterminate; unspecified for everything not captured by all other patterns
-  if ((length(male_found) > 0 & length(female_found) > 0) | 
-      (length(male_found) > 1) | 
-      (length(female_found) > 1) |
-      (length(mixed_found) > 0)) {
-    sex_category <- "MIXED"
-  } else if (length(male_found) == 1) {
-    sex_category <- "MALE"
-  } else if (length(female_found) == 1) {
-    sex_category <- "FEMALE"
-  } else if (length(atypical_found) == 1) {
-    sex_category <- "ATYPICAL"
-  } else if (length(unknown_found) == 1) {
-    sex_category <- "UNKNOWN"
-  } else if (length(indeterminate_found) == 1) {
+  if (length(attached_found) > 0) {
+    term_found <- toupper(attached_found[3])
+    sex_category <- ifelse(term_found %in% male_words, "MALE",
+                           ifelse(term_found %in% female_words, "FEMALE", "UNSPECIFIED"))
+  } else if (length(single_letter_found) > 0) {
+    sex_letter <- single_letter_found[2]
+    sex_category <- ifelse(sex_letter == "F", "FEMALE", "MALE")
+  } else if (indeterminate_found > 0) {
     sex_category <- "INDETERMINATE"
+  } else if ((male_found > 0 & female_found > 0) | mixed_found > 0) {
+    sex_category <- "MIXED"
+  } else if (male_found >= 1 & female_found == 0) {
+    sex_category <- "MALE"
+  } else if (female_found >= 1 & male_found == 0) {
+    sex_category <- "FEMALE"
+  } else if (atypical_found >= 1) {
+    sex_category <- "ATYPICAL"
+  } else if (unknown_found >= 1) {
+    sex_category <- "UNKNOWN"
   } else {
     sex_category <- "UNSPECIFIED"
   }
   
-  #Add notes with counts and extra information
-
-  notes_parts <- c()
-  # 1. Extract numeric counts with associated sex terms from the original value.
-  count_pattern <- "(?i)\\b\\d+\\s*(?:MALE|FEMALE|MACHO|MASCLE|FEMELLE|HEMBRA)S?\\b"
-  count_matches <- unlist(regmatches(original_value, gregexpr(count_pattern, original_value, perl=TRUE)))
-  
-  # Add counts to notes explicitly for MIXED cases
-  if (sex_category == "MIXED") {
-    if (length(count_matches) > 0) {
-      notes_parts <- c(notes_parts, paste(count_matches, collapse="; "))
-    }
-    
-  } else {
-    if (length(count_matches) > 0) {
-      notes_parts <- c(notes_parts, paste(count_matches, collapse="; "))
-    }
-  }
-  
-  # 3. Capture extra text after removing recognized tokens
-  extra_text <- value
-  extra_text <- gsub(male_pattern, "", extra_text, perl=TRUE)
-  extra_text <- gsub(female_pattern, "", extra_text, perl=TRUE)
-  extra_text <- gsub(count_pattern, "", extra_text, ignore.case=TRUE, perl=TRUE)
-  extra_text <- gsub("[[:punct:]]", " ", extra_text)  # remove punctuation
-  extra_text <- trimws(extra_text)
-  
-  # Remove any trailing numbers in mixed cases
-  if (sex_category == "MIXED") {
-    extra_text <- sub("(\\s*\\d+)+\\s*$", "", extra_text, perl=TRUE)
-  }
-  
-  if (nchar(extra_text) > 0) {
-    notes_parts <- c(notes_parts, extra_text)
-  }
-  
-  final_notes <- if (length(notes_parts) > 0) paste(notes_parts, collapse="; ") else NA
-  
-  
+  final_notes <- original_value  # Always store original text
   
   return(c(sex_category, final_notes))
 }
+
+# Apply function to data
 data <- sex %>% 
   rowwise() %>% 
   mutate(
@@ -122,6 +82,9 @@ data <- sex %>%
   ) %>% 
   ungroup() %>% 
   select(-result)
+
+
+
 #MIXED WILL BE RECORDS THAT HAVE MULTIPLE TERMS IN SEX COLUMN
   
   #INDETERMINATE RECORDS CONTAIN A QUESTION MARK
